84aa6de667a00ce708415c0bf85f83af1608ef37
Alex Rock Ancelet <pierstoval@gmail.com>
2023-10-18 16:17:18 +0200

    Remove unused module

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,11 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import { optimizeImports } from 'carbon-preprocess-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [vitePreprocess(), optimizeImports()],
+	preprocess: [vitePreprocess()],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
