74dde9502da190060d4109ab7b71454e054ea409
Pierstoval <Pierstoval@users.noreply.github.com>
2023-12-18 09:41:49 +0000

    deploy: fabb8ba81ddb7432bf095f68de0d308b632186b3

--- _app/immutable/chunks/stores.I4g1Ehhx.js
+++
@@ -1,1 +0,0 @@
-import"./Component.-lyeUsDq.js";import{d as e}from"./singletons.nwVSZzta.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
