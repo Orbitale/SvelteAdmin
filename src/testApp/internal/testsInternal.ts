ec516b4c8a5c704c500738680b9825b30c2d90e3
Alex Rock Ancelet <pierstoval@gmail.com>
2024-01-16 22:24:19 +0100

    Normalize testing fields & add Date field

--- src/testApp/internal/testsInternal.ts
+++ src/testApp/internal/testsInternal.ts
@@ -2,21 +2,33 @@ import { faker } from '@faker-js/faker';
 
 export type Test = {
 	id: string;
+	text_field: string;
+	textarea_field: string;
+	checkbox_field: boolean;
+	number_field: number;
+	toggle_field: boolean;
+	url_field: string;
+	path_field: string;
+	date_field: Date;
 	[key: string]: unknown;
 };
 
 const baseTests: Array<Test> = Array(10)
 	.fill(undefined)
-	.map(() => {
+	.map((): Test => {
+		const date = faker.date.anytime();
+		date.setHours(0, 0, 0);
+		date.setUTCSeconds(0, 0);
 		return {
 			id: faker.string.uuid(),
 			text_field: faker.music.songName(),
 			textarea_field: faker.lorem.lines(2),
-			checkbox_field: faker.number.int({ min: 0, max: 1 }) === 1,
-			number_field: faker.number.float(),
-			toggle_field: faker.number.int({ min: 0, max: 1 }) === 1,
+			checkbox_field: faker.datatype.boolean(),
+			number_field: faker.number.int({min: -10000, max: 10000}),
+			toggle_field: faker.datatype.boolean(),
 			url_field: faker.internet.url(),
-			path_field: faker.system.filePath().replace(/^\/[^/]+\//g, '/')
+			path_field: faker.system.filePath().replace(/^\/[^/]+\//g, '/'),
+			date_field: date
 		};
 	});
 
