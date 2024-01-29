b216795524eca4e52e5aa2d87e2d55240a00ab7e
Alex Rock Ancelet <pierstoval@gmail.com>
2024-01-30 00:24:09 +0100

    Fix DateField.svelte formatting issue

--- src/lib/themes/carbon/FormFieldsComponents/DateField.svelte
+++ src/lib/themes/carbon/FormFieldsComponents/DateField.svelte
@@ -12,6 +12,30 @@
 		value.setUTCSeconds(0, 0);
 		value = value.toISOString().substring(0, 10);
 	}
+
+	function dateFormatToRegex(dateFormat) {
+		return (
+			dateFormat
+				// Escape special characters in the date format string
+				.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+				// Convert date format to regular expression pattern
+				.replace('Y', '\\d{4}')
+				.replace('yyyy', '\\d{4}')
+				.replace('yy', '\\d{2}')
+				.replace('mm', '\\d{2}')
+				.replace('dd', '\\d{2}')
+				.replace('d', '\\d{2}')
+				.replace('M', '\\d')
+				.replace('yyyy', '\\d{4}')
+				.replace('hh', '\\d{2}')
+				.replace('mm', '\\d{2}')
+				.replace('m', '\\d{2}')
+				.replace('ss', '\\d{2}')
+				.replace('HH', '\\d{2}')
+				.replace('MM', '\\d{2}')
+				.replace('SS', '\\d{2}')
+		);
+	}
 </script>
 
 <DatePicker datePickerType="single" dateFormat={field.options.formFormat || 'Y-m-d'} {value}>
@@ -20,5 +44,6 @@
 		labelText={$_(field.label || field.name)}
 		helperText={field.options.help ? $_(field.options.help) : ''}
 		placeholder={field.options?.placeholder || 'yyyy-mm-dd'}
+		pattern={dateFormatToRegex(field.options.formFormat || 'Y-m-d')}
 	/>
 </DatePicker>
