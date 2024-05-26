0704a516e9520288a5a6845aa761ae710b0ea53a
Alex Rock Ancelet <pierstoval@gmail.com>
2024-05-26 21:18:02 +0200

    Add tests for DefaultField

---
+++ src/lib/themes/carbon/ViewFieldsComponents/DefaultField.test.ts
@@ -0,0 +1,41 @@
+import { describe, it, expect } from 'vitest';
+import {render} from '@testing-library/svelte'
+import '@testing-library/jest-dom';
+import { testOptions } from '$lib/TestOptions';
+import ComponentToTest from "./DefaultField.svelte";
+
+describe(
+    'DefaultField component',
+    () => {
+        it('can be instantiated with undefined', async () => {
+            const rendered = render(ComponentToTest, {
+                value: undefined,
+            });
+
+            const element = rendered.container;
+            expect(element).toBeDefined();
+            expect(element.innerHTML).toStrictEqual('<!--<DefaultField>-->');
+        });
+
+        it('can be instantiated empty string', async () => {
+            const rendered = render(ComponentToTest, {
+                value: undefined,
+            });
+
+            const element = rendered.container;
+            expect(element).toBeDefined();
+            expect(element.innerHTML).toStrictEqual('<!--<DefaultField>-->');
+        });
+
+        it('can be instantiated specific value', async () => {
+            const rendered = render(ComponentToTest, {
+                value: 'Some value',
+            });
+
+            const element = rendered.container;
+            expect(element).toBeDefined();
+            expect(element.innerHTML).toStrictEqual('Some value<!--<DefaultField>-->');
+        });
+    },
+    testOptions,
+);
