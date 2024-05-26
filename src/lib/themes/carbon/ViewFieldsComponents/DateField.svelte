ee573fc4f41392296f26d230788ddabfa883774c
Alex Rock Ancelet <pierstoval@gmail.com>
2024-05-26 18:07:55 +0200

    Fix a date parsing bug in DateField.svelte by using the luxon lib

--- src/lib/themes/carbon/ViewFieldsComponents/DateField.svelte
+++ src/lib/themes/carbon/ViewFieldsComponents/DateField.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+    import {DateTime} from 'luxon';
+
 	export let value: Date | string | undefined;
 
 	if (value instanceof Date) {
-		value.setHours(0, 0, 0);
-		value.setUTCSeconds(0, 0);
-		value = value.toISOString().substring(0, 10);
+        const luxonDate = DateTime.fromISO(value.toISOString());
+        value = luxonDate.toString().substring(0, 10);
 	}
 
 	if (value === undefined) {
---
+++ src/lib/themes/carbon/ViewFieldsComponents/DateField.test.ts
@@ -0,0 +1,43 @@
+import { describe, it, expect } from 'vitest';
+import {render} from '@testing-library/svelte'
+import '@testing-library/jest-dom';
+import { testOptions } from '$lib/TestOptions';
+import ComponentToTest from "./DateField.svelte";
+
+describe(
+    'DateField component',
+    () => {
+        it('can be instantiated with empty value', async () => {
+            const rendered = render(ComponentToTest, {
+                value: undefined,
+            });
+
+            const element = rendered.container;
+            expect(element).toBeDefined();
+            expect(element.innerHTML).toStrictEqual('<!--<DateField>-->');
+        });
+
+        it('can be instantiated with Date object', async () => {
+            const date = new Date('2024-01-01');
+            const rendered = render(ComponentToTest, {
+                value: date,
+            });
+
+            const element = rendered.container;
+            expect(element).toBeDefined();
+            expect(element.innerHTML).toStrictEqual(`2024-01-01<!--<DateField>-->`);
+        });
+
+        it('can be instantiated with string value', async () => {
+            const date = '2024-05-26 00:00:00 GMT+02:00';
+            const rendered = render(ComponentToTest, {
+                value: date,
+            });
+
+            const element = rendered.container;
+            expect(element).toBeDefined();
+            expect(element.innerHTML).toStrictEqual('2024-05-26<!--<DateField>-->');
+        });
+    },
+    testOptions,
+);
